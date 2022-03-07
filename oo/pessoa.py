877c472954bb793d93663506b05d66bd846b8b79
Longuin1234 <luisptavio.l.p@gmail.com>
2022-03-07 11:59:56 -0300

    Modificação sobrescrita de metodo

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -7,7 +7,7 @@ class Pessoa:
         self.filhos = list(filhos)
 
     def cumprimentar(self):
-        return f"Olá {id(self)}"
+        return f"Olá, meu nome é {self.nome}"
 
     @staticmethod
     def metodo_estatico():
@@ -18,7 +18,9 @@ class Pessoa:
         return (f'{cls} - olhos {cls.olhos}')
 
 class Homem(Pessoa):
-    pass
+    def cumprimentar(self):
+        cumprimentar_da_classe =super().cumprimentar()
+        return f"{cumprimentar_da_classe}. Aperto de mão"
 
 class Mutante(Pessoa):
     olhos = 3
@@ -27,7 +29,7 @@ class Mutante(Pessoa):
 
 if __name__ == '__main__':
     renzo = Mutante(nome="Jose")
-    p = Pessoa(renzo, nome= 'Luis')
+    p = Homem(renzo, nome= 'Luis')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
@@ -53,3 +55,5 @@ if __name__ == '__main__':
     print(isinstance(renzo, Homem))
     print(isinstance(renzo, Pessoa))
     print(renzo.olhos)
+    print(p.cumprimentar())
+    print(renzo.cumprimentar())
