4306795a9545f54858c8bd1812964219b3377c03
Longuin1234 <luisptavio.l.p@gmail.com>
2022-03-08 17:44:40 -0300

    modificações

--- atores.py
+++ atores.py
@@ -65,6 +65,7 @@ class Obstaculo(Ator):
 
 class Porco(Ator):
     _caracter_ativo = '@'
+    _caracter_destruido = '+'
 
 
 class DuploLancamentoExcecao(Exception):
@@ -96,7 +97,10 @@ class Passaro(Ator):
 
         :return: booleano
         """
-        return True
+        return not self._tempo_de_lancamento is None
+        # if self._tempo_de_lancamento is None:
+        #     return False
+        # return True
 
     def colidir_com_chao(self):
         """
@@ -132,7 +136,8 @@ class Passaro(Ator):
         :param tempo_de_lancamento:
         :return:
         """
-        pass
+        self._angulo_de_lancamento = angulo
+        self._tempo_de_lancamento = tempo_de_lancamento
 
 
 class PassaroAmarelo(Passaro):
@@ -141,3 +146,5 @@ class PassaroAmarelo(Passaro):
 
 class PassaroVermelho(Passaro):
     _caracter_ativo = 'V'
+    _caracter_destruido = 'v'
+    velocidade_escalar = 20
